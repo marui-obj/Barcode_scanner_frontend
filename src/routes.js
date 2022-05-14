a7705373326ecca2c78424075aafd27789fa0dcd
Marui <s6201012610044@email.kmutnb.ac.th>
2022-05-14 12:31:13 +0700

    Add path

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import PutAwayQrcode from "./pages/PutAwayQrcode";
 import DispatchIndex from "./pages/DispatchIndex"
 import DispatchDetail from "./pages/DispatchDetail";
 import AddNewItem from "./pages/AddNewItem";
+import PutAwayScanLocation from "./pages/PutAwayScanLocation";
 
 const routes = [
     {path: '/', exact: true, name: "Main-Menu", component: Menu},
     {path: '/put-away', exact: true, name: "Put-Away", component: PutAwayQrcode},
+    {path: '/put-away/scan-location', exact: true, name: "Put-Away-Location", component: PutAwayScanLocation},
     {path: '/add-item', exact: true, name: "Add-Item", component: AddNewItem},
     {path: '/dispatch', exact: true, name: "Dispatch-index", component: DispatchIndex},
     {path: '/dispatch/:id', exact: true, name: "Dispatch-detail", component: DispatchDetail}
