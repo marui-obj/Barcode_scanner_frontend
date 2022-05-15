1a3ef98d6578bd298392dca6e6633651eb565ebd
Marui <s6201012610044@email.kmutnb.ac.th>
2022-05-15 19:41:03 +0700

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import DispatchIndex from "./pages/DispatchIndex"
 import DispatchDetail from "./pages/DispatchDetail";
 import AddNewItem from "./pages/AddNewItem";
 import PutAwayScanLocation from "./pages/PutAwayScanLocation";
+import DispatchQrcode from "./pages/DispatchQrcode";
 
 const routes = [
     {path: '/', exact: true, name: "Main-Menu", component: Menu},
@@ -11,7 +12,8 @@ const routes = [
     {path: '/put-away/scan-location', exact: true, name: "Put-Away-Location", component: PutAwayScanLocation},
     {path: '/add-item', exact: true, name: "Add-Item", component: AddNewItem},
     {path: '/dispatch', exact: true, name: "Dispatch-index", component: DispatchIndex},
-    {path: '/dispatch/:id', exact: true, name: "Dispatch-detail", component: DispatchDetail}
+    {path: '/dispatch/:id', exact: true, name: "Dispatch-detail", component: DispatchDetail},
+    {path: '/dispatch/scan', exact: true, name: "Dispatch-scan", component: DispatchQrcode}
 ]
 
 export default routes;
