b0ef03e0d8df897577e1a1f4a244eaf5ee391f5c
Marui <s6201012610044@email.kmutnb.ac.th>
2022-05-15 22:35:21 +0700

    add id to display

--- src/components/Dispatch-detail-list/index.js
+++ src/components/Dispatch-detail-list/index.js
@@ -9,6 +9,7 @@ const DispatchDetailList = ({products}) => {
                 <div className="dispatch-detail-list" style={{margin: "10px"}} key={product._id}>
                 <Link to={'/dispatch/scan'} state={{product, task}} style={{ textDecoration: 'none' }}>
                     <Button style={{flexDirection: 'column', backgroundColor: "#C4C4C4", alignItems: "flex-start", width: "100%"}}>
+                        <div style={{display: "block"}}>Id: {product._id}</div>
                         <div style={{display: "block"}}>Name: {product.name} </div>
                         <div style={{display: "block"}}>Location Room: {product.location}</div>
                     </Button>
